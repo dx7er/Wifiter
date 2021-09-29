3a849033991a0135c11802763e411f9de09f9524
Saqlain Naqvi <79792270+naqviO7@users.noreply.github.com>
2021-09-29 17:56:28 +0500

    Update version3.sh

--- version3.sh
+++ version3.sh
@@ -1,14 +1,15 @@
-####################################################
-#		      Wifiter 		           #	
-####################################################
-#   Bash Script to Perfrom DOS on Wifi-Router       #
-#      Version: 3.0				    #
-#	Author: naqviO7 *saqlainnaqvi*              #
-#####################################################
+#!/bin/bash
+
+#desclaimer/description
+#################################################
+#		      Wifiter 		        #	
+#################################################
+#   Bash Script to Perfrom DOS on Wifi-Router   #
+#      Version: 3.0				#
+#	Author: naqviO7 *saqlainnaqvi*          #
+#################################################
 
 #STARTOFCODE
-
-#!/bin/bash
 
 #colors used in this script
 red='\e[1;31m'
